46d01d56dea2840a42ab185a360bae1a30a53888
me <me@me.me.com>
2020-05-01 14:52:26 -0700

    create html skeleton and js skeleton [comment out solution]

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// function binaryToDecimal(binary){
+//   return binary.split("").reverse().reduce(function (accumulator,digit,index){
+//     return accumulator + (parseInt(digit) * Math.pow(2, index));
+//   },0);
+// }
+
+
+// function parseAny(digit){
+ 
+//   if(parseInt(digit)>=0){
+//     return parseInt(digit)
+//   }
+//   var unicode = digit.toUpperCase().charCodeAt()
+//   return unicode - 55;
+// }
+
+// function hexadecimalToDecimal(hexadecimal){
+//   return hexadecimal.split("").reverse().reduce(function (accumulator,digit,index){
+//     return accumulator + (parseAny(digit) * Math.pow(16, index));
+//   },0);
+// }
+
+// function anyBaseToDecimal(anybase,base){
+//   return anybase.split("").reverse().reduce(function (accumulator,digit,index){
+//     return accumulator + (parseAny(digit) * Math.pow(base, index));
+//   },0);
+// }
+
+
+$(document).ready(function(){
+  
+});
