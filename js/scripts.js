12394b54da00daea0bc52b8ade732b8c1cc20d05
me <me@me.me.com>
2020-05-04 14:04:46 -0700

    finish the project

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,27 @@
 
 function anyBaseToDecimal(anybase,base){
  return anybase.split("").reverse().reduce(function (accumulator,digit,index){
-     var value = (parseInt(digit) >=0) ? parseInt(digit) : digit.toUpperCase().charCodeAt() - 55; 
-     return accumulator + (value * Math.pow(base, index));
+     var digitvalue = (parseInt(digit) >=0) ? parseInt(digit) : digit.toUpperCase().charCodeAt() - 55; 
+     return accumulator + (digitvalue * Math.pow(base, index));
    },0);
  }
 
 
 $(document).ready(function(){
-    $("binaryform").submit()
+    $("#binaryform").submit(function(e){
+      e.preventDefault();
+      var binary = $("#binary").val();
+      $("#binaryresult").text(anyBaseToDecimal(binary,2));
+    })
+    $("#hexadecimalform").submit(function(e){
+      e.preventDefault();
+      var hexadecimal = $("#hexadecimal").val();
+      $("#hexadecimalresult").text(anyBaseToDecimal(hexadecimal,16));
+    })
+    $("#anybaseform").submit(function(e){
+      e.preventDefault();
+      var anybase = $("#anybase").val();
+      var base =$("#base").val();
+      $("#anybaseresult").text(anyBaseToDecimal(anybase,base));
+    })
 });
