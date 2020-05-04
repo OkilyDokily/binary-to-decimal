03c3ea024333bc9b9bd6964117d438fce2707995
me <me@me.me.com>
2020-05-04 13:54:25 -0700

    further refactor business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
 //business logic
-function parseAny(digit){
-  if(parseInt(digit)>=0){
-    return parseInt(digit)
-  }
-  var unicode = digit.toUpperCase().charCodeAt()
-  return unicode - 55;
-}
 
 function anyBaseToDecimal(anybase,base){
  return anybase.split("").reverse().reduce(function (accumulator,digit,index){
-     return accumulator + (parseAny(digit) * Math.pow(base, index));
+     var value = (parseInt(digit) >=0) ? parseInt(digit) : digit.toUpperCase().charCodeAt() - 55; 
+     return accumulator + (value * Math.pow(base, index));
    },0);
  }
 
