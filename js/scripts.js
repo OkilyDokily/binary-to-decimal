14ee612f645541de7c62aff232d594c18427cfe0
me <me@me.me.com>
2020-05-04 13:28:00 -0700

    refactor the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,3 @@
-// function binaryToDecimal(binary){
-//   return binary.split("").reverse().reduce(function (accumulator,digit,index){
-//     return accumulator + (parseInt(digit) * Math.pow(2, index));
-//   },0);
-// }
-
-
-// function parseAny(digit){
- 
-//   if(parseInt(digit)>=0){
-//     return parseInt(digit)
-//   }
-//   var unicode = digit.toUpperCase().charCodeAt()
-//   return unicode - 55;
-// }
-
-// function hexadecimalToDecimal(hexadecimal){
-//   return hexadecimal.split("").reverse().reduce(function (accumulator,digit,index){
-//     return accumulator + (parseAny(digit) * Math.pow(16, index));
-//   },0);
-// }
-
-// function anyBaseToDecimal(anybase,base){
-//   return anybase.split("").reverse().reduce(function (accumulator,digit,index){
-//     return accumulator + (parseAny(digit) * Math.pow(base, index));
-//   },0);
-// }
-
-
-function parseBinaryDigit(digit){
-  return parseInt(digit)
-}
-
 function parseAny(digit){
   if(parseInt(digit)>=0){
     return parseInt(digit)
@@ -39,20 +6,10 @@ function parseAny(digit){
   return unicode - 55;
 }
 
-function convertHighestPlaceToDecimal(binary){
-  var highest = binary.split("")[0];
-  return parseBinaryDigit(highest) * Math.pow(2,binary.length-1);
-}
-
-function addSecondAndFirstDecimal(binary){
-  var highest = convertHighestPlaceToDecimal(binary);
-  var second = parseBinaryDigit(binary.split("")[1]);
-  return highest + second;
-}
 
 function binaryToDecimal(binary){
  return binary.split("").reverse().reduce(function(accumulator,digit,index){
-    return accumulator + (parseBinaryDigit(digit) * Math.pow(2,index));
+    return accumulator + (parseInt(digit) * Math.pow(2,index));
   },0);
 }
 
